5c102fb36df9d291416b1613a713436db3dc3379
sadambk <sadamaisha@yahoo.com>
2016-03-05 02:32:08 +0800

    When button pressed, count number of characters in string.

--- app/src/main/java/com/example/salis/c1541078bj_task1/MainActivity.java
+++ app/src/main/java/com/example/salis/c1541078bj_task1/MainActivity.java
@@ -8,8 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText EditTextInput;
+    String strInput;
+    Button btn;
+    TextView txtLenght;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+         EditTextInput= (EditText)findViewById(R.id.editText);
+        txtLenght = (TextView)findViewById(R.id.textView2);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                //strInput = EditTextInput.getText().toString();
+               txtLenght.getText().toString();
+
+                if (EditTextInput == txtLenght)
+                {
+                    Toast.makeText(getApplicationContext(), "Your Guess is correct", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Your Guess is wrong!!!", Toast.LENGTH_LONG).show();
             }
-        });
-    }
+        };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+});
     }
 }
